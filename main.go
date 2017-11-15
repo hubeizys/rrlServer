dac72e862aa4a3fe9bbd580636c79138265006f5
PC-201611301853\Administrator <124296140@qq.com>
2017-11-15 20:28:57 +0800

    asdasd

--- main.go
+++ main.go
@@ -6,10 +6,24 @@ import (
 	"github.com/astaxie/beego"
 	_ "nepliteApi/models"
 	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/astaxie/beego/logs"
 )
 func init() {
-	orm.RegisterDriver("sqlite3", orm.DRSqlite)
-	orm.RegisterDataBase("default", "sqlite3", "neplite.db")
+	dbType := beego.AppConfig.String("db_type")
+	dbAlias := beego.AppConfig.String(dbType + "::db_alias")
+	dbName := beego.AppConfig.String(dbType + "::db_name")
+	dbUser := beego.AppConfig.String(dbType + "::db_user")
+	dbPwd := beego.AppConfig.String(dbType + "::db_pwd")
+	dbPort := beego.AppConfig.String(dbType + "::db_port")
+	dbHost := beego.AppConfig.String(dbType + "::db_host")
+	//orm.RegisterDriver("sqlite3", orm.DRSqlite)
+	orm.RegisterDriver(dbType, orm.DRMySQL)
+	//orm.RegisterDataBase("default", "sqlite3", "neplite.db")
+	// dbCharset := beego.AppConfig.String(dbType + "db_charset")
+	dataSource := dbUser + ":" + dbPwd + "@tcp(" +dbHost + ":"+ dbPort +")/" + dbName + "?charset=utf8"
+	logs.Info("%s==== %s", dbAlias, dataSource)
+	orm.RegisterDataBase(dbAlias, dbType, dataSource)
 	orm.RunSyncdb("default", false, true)
 }
 
